0aa9d4d984a72968c4593e4c8a174549bf4e36da
lawrence_muema_m <lawrencemuema96@gmail.com>
2023-05-16 13:14:36 +0300

    our first program as Cloud Engineers

---
+++ program1.py
@@ -0,0 +1,14 @@
+#computer asks us
+# what is your name?
+# what is your age?
+# what is your country?
+
+#we give a response.
+
+#we need feedback
+#welcome name, i see you are from country, and you are age years old
+pickName = input("what is your name")
+
+print(pickName)
+print("name")
+print(32)
